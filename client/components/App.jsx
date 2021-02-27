6e5d4727322578c0c298e86e449b6ce23761e2f9
ChrisHolley <asiholley@gmail.com>
2021-02-26 16:20:52 -0800

    + hardcode html of product overview + styles

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,9 +2,47 @@ import React from 'react';
 
 const App = (props) => (
   <div>
-    Hello World from App
-    <div id="productDetail">
-      this is the product view
+    <div id="productContainer" className="border">
+      <div id="productImageView" className="border">
+        <div id="imgViewerComponent">
+          <img id="imgNormalView" src="https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1511&q=80"></img>
+        </div>
+        <div id="productView">
+          this is the product view
+          <div>this is the rating component</div>
+          <span>CATEGORY</span>
+          <h1>Expanded product name</h1>
+          <div>
+            <span>$price</span>
+            <span> discounted price</span>
+          </div>
+          <div>
+            STYLE > SELECTED STYLE
+          </div>
+          <div className="styleView">
+            <div>style row 1</div>
+            <div>style row 2</div>
+          </div>
+          <select name="SELECT SIZE">
+            <option value="">-</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+          <select name="QUANTITY">
+            <option value="1">1</option>
+            <option value="2">2</option>
+          </select>
+          <div className="flexSpaceBetween">
+            <div id="addToBag" className="flexAuto1">
+              <span className="">ADD TO BAG</span>
+              <span className=""> +</span>
+            </div>
+            <div className="star">
+              STAR
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 
   </div>

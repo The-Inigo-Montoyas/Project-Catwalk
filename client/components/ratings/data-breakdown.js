7d34b09a86a0c850a037c5100ab962fbaca8c8a1
Pete Cowles <kilgore9trout@gmail.com>
2021-02-28 14:29:51 -0800

    trying to get the bars to render

--- client/components/ratings/data-breakdown.js
+++ client/components/ratings/data-breakdown.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import sampleObj from '../Sample_data/single_product_metadata.js'
+import StarGraph from './StarGraph.js'
 
 class Breakdown extends React.Component {
   constructor (props) {
@@ -27,7 +28,7 @@ class Breakdown extends React.Component {
     //find the percentage of recommends
     let recommend = Number.parseInt(dataObj.recommended.true);
     let notRecommend = Number.parseInt(dataObj.recommended.false);
-    results.pctRecommend = recommend / (recommend + notRecommend);
+    results.pctRecommend = Math.round(100 * recommend / (recommend + notRecommend));
 
     //find the number of each star rating and the weighted average
     results.stars = {};
@@ -38,7 +39,7 @@ class Breakdown extends React.Component {
       results.totalReviews += results.stars[i];
       results.weightedTotal += results.stars[i] * Number.parseInt(i);
     }
-    results.weightedAvg = results.weightedTotal / results.totalReviews;
+    results.weightedAvg = (Math.round(10 * results.weightedTotal / results.totalReviews)) / 10;
 
     //save all the characteristics into the new obj
     results.characteristics = {};
@@ -55,12 +56,11 @@ class Breakdown extends React.Component {
     return (
       <div>
         <h1>{goodData.weightedAvg}</h1>
-        <h3>{goodData.pctRecommend} of reviews recommend this product</h3>
-        <h3>5 stars {goodData.stars[5]}/{goodData.totalReviews}</h3>
-        <h3>4 stars {goodData.stars[4]}</h3>
-        <h3>3 stars {goodData.stars[3]}</h3>
-        <h3>2 stars {goodData.stars[2]}</h3>
-        <h3>1 stars {goodData.stars[1]}</h3>
+        <div>
+          <span className="star fa fa-star">Stars go here</span>
+        </div>
+        <p>{goodData.pctRecommend}% of reviews recommend this product</p>
+        <StarGraph stars={goodData.stars} reviews={goodData.totalReviews}/>
       </div>
     )
   }

5a7cfb51670be3b21e7407e24885e9825891d3fc
ChrisHolley <asiholley@gmail.com>
2021-03-01 15:12:21 -0800

    + add GET req to init app with a product

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,39 +1,67 @@
+const axios = require('axios');
+const TOKEN = require('../../config.js');
+
 import React from 'react';
+import { useState } from 'react';
 import RatingsApp from './ratings/RatingsApp.jsx';
 import QuestionsList from './Questions/QuestionsList.jsx';
 import ProductDetailsView from './productDetails/ProductDetailsView.jsx';
 import ProductDescription from './productDetails/ProductDescription.jsx';
 
-const App = () => (
-  <div className="">
-    <div className="gridContainer gridMainTemplate">
-      <div className="gridSpacer" />
-      <div className="header">
-        <div className="inner-header">
-          <img className="logo-img" src="./img/ankylosaur1.jpg" alt="logo" />
-          <p className="logo">Agile Creations</p>
-          <input className="logo-search" placeholder="search..." />
+const App = () => {
+  const [product, setProduct] = useState([]);
+
+  const getOneProduct = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products', {
+      headers: {
+        Authorization: TOKEN,
+      },
+      params: {
+        count: 1,
+      },
+    })
+      .then((res) => {
+        setProduct(res.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  useState(getOneProduct);
+
+
+  return (
+    <div className="">
+      <div className="gridContainer gridMainTemplate">
+        <div className="gridSpacer" />
+        <div className="header">
+          <div className="inner-header">
+            <img className="logo-img" src="./img/ankylosaur1.jpg" alt="logo" />
+            <p className="logo">Agile Creations</p>
+            <input className="logo-search" placeholder="search..." />
+          </div>
         </div>
+        <div className="gridSpacer" />
+        <div className="gridSpacer" />
+        <ProductDetailsView product={product}/>
+        <div className="gridSpacer" />
+        <div className="gridSpacer" />
+        <ProductDescription />
+        <div className="gridSpacer" />
+        <div className="gridSpacer" />
+        <div id="questions-answers">
+          <QuestionsList />
+        </div>
+        <div className="gridSpacer" />
+        <div className="gridSpacer" />
+        <div id="reviews-ratings">
+          <RatingsApp />
+        </div>
+        <div className="gridSpacer" />
       </div>
-      <div className="gridSpacer" />
-      <div className="gridSpacer" />
-      <ProductDetailsView />
-      <div className="gridSpacer" />
-      <div className="gridSpacer" />
-      <ProductDescription />
-      <div className="gridSpacer" />
-      <div className="gridSpacer" />
-      <div id="questions-answers">
-        <QuestionsList />
-      </div>
-      <div className="gridSpacer" />
-      <div className="gridSpacer" />
-      <div id="reviews-ratings">
-        <RatingsApp />
-      </div>
-      <div className="gridSpacer" />
     </div>
-  </div>
-);
+  );
+};
 
 export default App;
--- client/components/productDetails/ProductDetailsView.jsx
+++ client/components/productDetails/ProductDetailsView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import ProductDetails from './ProductDetails.jsx';
 
 const ProductDetailsView = () => (
   <div id="productContainer" className="border">
@@ -6,41 +7,7 @@ const ProductDetailsView = () => (
       <div id="imgViewerComponent">
         { <img id="imgNormalView" src="https://images.unsplash.com/photo-1553830591-d8632a99e6ff?ixlib=rb-1.2.1&auto=format&fit=crop&w=1511&q=80"></img> }
       </div>
-      <div id="productView">
-        this is the product view
-        <div>this is the rating component</div>
-        <span>CATEGORY</span>
-        <h1>Expanded product name</h1>
-        <div>
-          <span>$price</span>
-          <span> discounted price</span>
-        </div>
-        <div>
-          STYLE &gt; SELECTED STYLE
-        </div>
-        <div className="styleView">
-          <div>style row 1</div>
-          <div>style row 2</div>
-        </div>
-        <select id="sizeBar" name="SELECT SIZE">
-          <option value="">-</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-        </select>
-        <select id="qtyBar" className="m20" name="QUANTITY">
-          <option value="1">1</option>
-          <option value="2">2</option>
-        </select>
-        <div className="flexSpaceBetween">
-          <div id="addToBag" className="m20">
-            <span>ADD TO BAG</span>
-            <span className=""> +</span>
-          </div>
-          <div className="star m20">
-            *
-          </div>
-        </div>
-      </div>
+      <ProductDetails />
     </div>
   </div>
 );
